4c6f707c4b9750a6846a8dd683f0460f9bbb2116
Ryan Brott <rcbrott@gmail.com>
2024-03-30 11:34:25 -0700

    v0.1.4 release

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,7 +56,7 @@ publishing {
         create<MavenPublication>("meepmeep") {
             groupId = "com.acmerobotics.roadrunner"
             artifactId = "MeepMeep"
-            version = "0.1.3"
+            version = "0.1.4"
 
             from(components["java"])
             artifact(sourcesJar)
